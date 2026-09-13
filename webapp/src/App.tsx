3f7efb579ddab9a354f1ccc676274a49d699b3e9
JC <sssssssnk7@gmail.com>
2026-09-13 14:54:19 -0300

    Added react router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,18 @@
-import { TrpcProvider } from "./lib/trpc"
-import { AllIdeasPage } from "./pages/AllIdeasPage"
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { getAllIdeasRoute, getViewIdeaRoute, viewIdeaRouteParams } from './lib/routes'
+import { TrpcProvider } from './lib/trpc'
+import { AllIdeasPage } from './pages/AllIdeasPage'
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom'
+import { getViewIdeaRoute } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
+
 export const AllIdeasPage = () => {
   const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
   if (isLoading || isFetching) {
@@ -11,10 +14,9 @@ export const AllIdeasPage = () => {
     <div>
       <h1>All Ideas</h1>
       {data?.ideas.map((idea) => (
-        <div key={idea.nick}>
-          <h2>{idea.name}</h2>
-          <p>{idea.description}</p>
-        </div>
+        <h2>
+          <Link to={getViewIdeaRoute({ ideaNick: idea.nick })}>{idea.name}</Link>
+        </h2>
       ))}
     </div>
   )
