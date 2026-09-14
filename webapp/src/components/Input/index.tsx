5a0fb1dd5b08792ebc1fa8790bf25c648a6dce09
JC <sssssssnk7@gmail.com>
2026-09-14 18:17:19 -0300

    Add formik integration with Input and Textarea components in NewIdeaPage

---
+++ webapp/src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import { type FormikProps } from 'formik'
+export const Input = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
+  const value = formik.values[name]
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          void formik.setFieldValue(name, e.target.value)
+        }}
+        value={value}
+        name={name}
+        id={name}
+      />
+    </div>
+  )
+}
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,4 +1,34 @@
+import { useFormik } from 'formik'
+import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
+import { Textarea } from '../../components/TextArea'
 export const NewIdeaPage = () => {
-  return <Segment title="New Idea">Form will be here...</Segment>
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      nick: '',
+      description: '',
+      text: '',
+    },
+    onSubmit: (values) => {
+      console.info('Submitted', values)
+    },
+  })
+
+  return (
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          formik.handleSubmit()
+        }}
+      >
+        <Input name="name" label="Name" formik={formik} />
+        <Input name="nick" label="Nick" formik={formik} />
+        <Input name="description" label="Description" formik={formik} />
+        <Textarea name="text" label="Text" formik={formik} />
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
+  )
 }
