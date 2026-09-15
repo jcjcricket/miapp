1f618ff6409efe26351feacdfe2888a4ea537465
JC <sssssssnk7@gmail.com>
2026-09-14 21:26:03 -0300

    Re-Implement TRPC integration and add ideas data structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,13 @@
-import * as trpcExpress from '@trpc/server/adapters/express'
 import cors from 'cors'
 import express from 'express'
-import { trpcRouter } from './trpc'
-
+import { applyTrpcToExpressApp } from './lib/trpc'
+import { trpcRouter } from './router'
 const expressApp = express()
 expressApp.use(cors())
-
-expressApp.get('/', (req, res) => {
-  res.send('Hello, World!')
+expressApp.get('/ping', (req, res) => {
+  res.send('pong')
 })
-
-expressApp.use(
-  '/trpc',
-  trpcExpress.createExpressMiddleware({
-    router: trpcRouter,
-    createContext: () => ({}),
-  })
-)
-
-const PORT = process.env.PORT || 3000
-expressApp.listen(PORT, () => {
-  console.info(`Server is running on port http://localhost:${PORT}`)
+applyTrpcToExpressApp(expressApp, trpcRouter)
+expressApp.listen(3000, () => {
+  console.info('Listening at http://localhost:3000')
 })
